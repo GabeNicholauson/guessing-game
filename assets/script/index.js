06799b1a2e70df000ada664ef38dafb159a21b0c
Gabriel <gnicholauson@gmail.com>
2022-11-06 19:13:27 -0600

    added guess mechanic

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,45 @@
 'use strict';
+
+let guess = document.querySelector('.guess');
+let guessButton = document.querySelector('.enter-guess');
+let result = document.querySelector('.result');
+let totalGuesses = document.querySelector('.total-guesses');
+let guessCount = 0;
+let number = getRandomNum();
+
+
+/***************************
+    Inputs
+***************************/
+
+guessButton.addEventListener('click', () => {
+    countGuess();
+    checkGuess(guess.value.trim());
+});
+console.log(number);
+/***************************
+    Functions
+***************************/
+
+function getRandomNum() {
+    return Math.trunc(Math.random() * 50 + 1);
+}
+
+function countGuess() {
+    guessCount++;
+    totalGuesses.innerHTML = `Guesses: ${guessCount}`;
+}
+
+function checkGuess(playerGuess) {
+    if (playerGuess === '') {
+        result.value = 'Please enter a guess';
+    } else if (isNaN(playerGuess)) {
+        result.value = 'Please enter a number';
+    } else if (playerGuess > number) {
+        result.value = 'Too high';
+    } else if (playerGuess < number) {
+        result.value = 'Too low'
+    } else if (Number(playerGuess) === number) {
+        result.value = `You guessed the number in ${guessCount} tries!`;
+    }
+}
